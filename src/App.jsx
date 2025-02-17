11e4f748f10866bce7414e77cd045d69a7821712
LorensZhou <lorenszhou@yahoo.com>
2025-02-17 21:50:24 +0100

    bestand App.jsx gewijzigd titel pagina

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import './App.css'
 function App() {
 
   return (
-    <>
-        <h1>Fruitmand bezorgservice</h1>
-    </>
+      <>
+          <h1>Fruitmand bezorgservice</h1>
+          <h1>Fruitmand bezorgservice</h1>
+
+      </>
   )
 }
 
